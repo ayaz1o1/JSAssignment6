63ee0ec24c6ab18de121610d184d4a9118664479
ayaz1o1 <ayaz_ansari1983@yahoo.com>
2020-06-21 15:00:35 +0500

    Update app.js
    ques 5 added

--- app.js
+++ app.js
@@ -335,3 +335,23 @@ function   dicefun(){
   var newNum = Math.floor(improvedNum);
   document.write("Random dice value: " + newNum);
 }
+
+
+//5, random coin value
+
+document.write("<input onclick='dicefun()' type='button' value='Coin toss'>");
+
+function   dicefun(){
+  // var randomnumber = prompt(" enter any number form 1 to 10");
+  var improvedNum = Math.random() * 2 + 1;
+  var newNum = Math.floor(improvedNum);
+  if ( newNum == 2){
+    document.write("Random coin value : Heads " + newNum);
+
+  }
+
+  else {
+    document.write("Random coin value : Tails " + newNum);
+
+  }
+}
